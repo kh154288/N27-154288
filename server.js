df50fb84612de4b9bf90e86e6e7415e1616ee946
kh154288 <kh154288@students.berufskolleg-borken.de>
2022-02-03 09:09:37 +0100

    Benutzerdaten entgegen nehmen

--- server.js
+++ server.js
@@ -1,3 +1,38 @@
+// Programme verarbeiten oft Objekte der realen Welt.
+// Objekte haben Eigenschaften,
+// In unserem Bankingprogrammm interesieren uns Objekte, 
+// wie zb. Kunde, Konto, Filiale, Bankautomat,...
+//Alle Kunden unserer Bank haben dieselben Eigenschaften, 
+//aber unterschiedliche Eigenschaftswerte
+
+class Kunde{
+    constructor(){
+        this. IdKunde
+        this. Nachname
+        this. Vorname
+        this. Kennwort
+        this. Kontostand
+        this. Geburtsdatum
+        this. Mail
+    }
+}
+
+// Von der Kundenklasse wird eine konkrete Instanz 
+// gebildet.
+
+let kunde = new Kunde()
+
+// Die konkrete Instanz bekommt Eigenschaftswerte 
+// zugewiesen.
+
+kunde.IdKunde = "154288"
+kunde.Nachname = "Kiff"
+kunde.Vorname = "Lena"
+kunde.Geburtsdatum = "13.09.2005"
+kunde.Mail = "kiff@gmail.com"
+kunde.Kennwort = "12345"
+
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const meineApp = express()
@@ -11,6 +46,22 @@ const server = meineApp.listen(process.env.PORT || 3000, () => {
 
 meineApp.get('/',(browserAnfrage, serverAntwort, next) => {              
     serverAntwort.render('index.ejs', {})          
+})
+
+meineApp.post('/login',(browserAnfrage, serverAntwort, next) => {      
+    
+    const idKunde = browserAnfrage.body.IdKunde
+    const kennwort = browserAnfrage.body.Kennwort
+
+    console.log("ID des Kunden: " +  idKunde)
+    console.log("Kennwort des Kunden: " + kennwort)
+
+    if(idKunde == kunde.idKunde){
+        serverAntwort.render('index.ejs', {})  
+
+    }
+
+    serverAntwort.render('login.ejs', {})          
 })
 
 //Wenn die login-Seite im Browser aufgerufem wird, ...
@@ -27,3 +78,5 @@ meineApp.get('/login',(browserAnfrage, serverAntwort, next) => {
 meineApp.post('/login',(browserAnfrage, serverAntwort, next) => {  
      serverAntwort.render('index.ejs', {})          
 })
+
+
