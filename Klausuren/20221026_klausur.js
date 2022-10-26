ef8e9ff027d108d0ac76fa1a14f48e4b9bc96d1d
kh154288 <kh154288@students.berufskolleg-borken.de>
2022-10-26 10:26:41 +0200

    ...

--- Klausuren/20221026_klausur.js
+++ Klausuren/20221026_klausur.js
@@ -1,14 +1,3 @@
 console.log("Mein Name: Hannah Klöpper")
+console.log("========================")
 
-let a = "10"
-let b = 10
-
-console.log("Der Wert von a ist" + a )
-
-if(a == b){
-    console.log("a und b sind wertgleich, aber nicht typgleich")
-}
-
-console.log( "Übung 3 - Prüfung auf Wert- (und Typ-) Gleichheit:")
-
-console.log( "Übung 4 - Prüfung auf Wert- (und Typ-) Gleichheit:")
